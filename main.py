cbf674fbefa5f87bacc6e1ed500a39f356b10e2b
Stormtorch002 <59403023+Stormtorch002@users.noreply.github.com>
2021-03-09 09:32:46 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import webbrowser
+import csv
+import time
+import datetime
+
+
+def get_dt_obj(row):
+	t = row[1]
+	t = t.split(':')
+	h, m = int(t[0]), int(t[1])
+	t_obj = datetime.datetime.now()
+	return t_obj.replace(hour=h, minute=m)
+
+
+with open('./schedule.csv') as f:
+	reader = csv.reader(f, delimiter='\t')
+	reader = reader.sort(key=get_dt_obj)
+
+
+	for row in reader:
+		t_obj = get_dt_obj(row)
+		now = datetime.datetime.now()
+
+		if t_obj < now:
+			continue
+
+		delta = (t_obj - now).total_seconds()
+		time.sleep(delta)
+
+		webbrowser.open(row[0])
